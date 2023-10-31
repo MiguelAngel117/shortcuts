19f393a002b22db85e2b08735790c01872e09d40
MIguel Angel <miguel.choconta@uptc.edu.co>
2023-10-31 15:13:48 -0500

    first commit

---
+++ input.c
@@ -0,0 +1,27 @@
+#inc <stdio.h>
+#inc <stdlib.h>
+#inc <string.h>
+
+ v greetUser( ch *name) {
+    pf("¡Hola, %s!\n", name);
+}
+
+ ch* getUserName() {
+    ch* name;
+    name = (char*) mll (50);
+    i (!name) {
+        pf("Error al asignar memoria.\n");
+        exit(1);
+    }
+    pf("Ingrese su nombre: ");
+    scanf("%s", name);
+    r name;
+}
+
+ in main() {
+    char* userName;
+    userName = getUserName();
+    greetUser(userName);
+    free(userName);
+    r 0;
+}
